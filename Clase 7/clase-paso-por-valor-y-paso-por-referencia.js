c31f184fd8f82c1d1b9c5c7d2e01e2a59be732d1
Juliana Castillo <jcastilloaraujo@icloud.com>
2023-11-24 17:28:04 -0500

    Clase de panes - Clase 7 🚀

---
+++ Clase 7/clase-paso-por-valor-y-paso-por-referencia.js
@@ -0,0 +1,26 @@
+// Paso por valor
+
+let x = 1
+let y = "Hola"
+let z = null
+
+let a = x
+let b = y
+let c = z
+
+console.log(x, y, z, a, b, c)
+
+a = 12
+b = "Platzi"
+c = undefined
+
+// Paso por referencia
+
+let frutas = ["manzana"]
+frutas.push("pera")
+console.log(frutas)
+
+let panes = ["🥐​"]
+let copiaDePanes = panes
+panes.push("🥖​")
+console.log(panes, copiaDePanes)
