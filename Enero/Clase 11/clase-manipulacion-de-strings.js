87cd0ec23b2959ea169b89d59f736246aed578a6
jlianacastillo <jcastilloa@ucundinamarca.edu.co>
2024-02-01 00:51:44 -0500

    Organizo carpeta Enero

---
+++ Enero/Clase 11/clase-manipulacion-de-strings.js
@@ -0,0 +1,7 @@
+// String primitivos
+
+const stringPrimitivo = 'Soy un string primitivo'
+console.log(typeof stringPrimitivo)
+
+const stringPrimitivoTambien = String('Soy un string primitivo tambien')
+console.log(typeof stringPrimitivoTambien)
