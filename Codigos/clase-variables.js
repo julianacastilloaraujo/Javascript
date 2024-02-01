8afed9f23945c30b73ab22387bd7273701d9a6e8
jlianacastillo <jcastilloa@ucundinamarca.edu.co>
2024-02-01 00:56:38 -0500

    Clase de variables en #javascript 😜

---
+++ Codigos/clase-variables.js
@@ -0,0 +1,14 @@
+let cajaDeAndy = "Woody"
+console.log(cajaDeAndy)
+
+// Lo NO permitido
+let c = "Woody"
+let cda = "Woody"
+let pcAndy = "Woody"
+
+
+// Lo permitido
+let primerTrasteoDeAndy = "Woody"
+let urlDelUsuario = "http://www.google.com"
+let idDelUsuario = "123456789"
+
