73998e433596042bbc88965d07be34609f730403
jlianacastillo <jcastilloa@ucundinamarca.edu.co>
2024-02-15 10:10:51 -0500

    Cambios

--- Codigos/clase-number.js
+++ Codigos/clase-number.js
@@ -2,3 +2,18 @@
 const entero = 42
 const decimal = 3.14
 console.log(typeof entero, typeof decimal)
+
+// 2. Notacion cientifica
+const cientifico = 5e3
+
+// 3. Infinitos y NaN
+const infinito = Infinity
+const noEsUnNumero = NaN
+
+// Operaciones aritmeticas
+// 1. Suma, Resta, Multiplicacion, Division
+const suma = 3 + 4
+const resta = 4 - 4
+const Multiplicacion = 4 * 7
+const division = 16 / 2
+
